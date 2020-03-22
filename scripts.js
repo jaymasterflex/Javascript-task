0184237731586b627d235e6ad55bc56435df58ee
Justin <50380997+jaymasterflex@users.noreply.github.com>
2020-03-22 16:21:05 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+let name = "Justin Obinna"
+console.log(name);
+let courses = ['html','css','javascript','node js','python']
+console.log(courses);
+let len = courses.length
+
+for (let i=1; i<=200; i+=2) {
+  if(len %2 !== 0){
+  console.log(i);
+  }
+}
